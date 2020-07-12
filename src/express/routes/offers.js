f908c76ef9621c309f48472632e463198af38734
novitckas <novitckas@selectel.ru>
2020-07-12 17:48:31 +0300

    Добавил пропущенный путь

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -6,6 +6,7 @@ const offersRouter = new Router();
 
 offersRouter.get(`/category/:id`, (req, res) => res.send(req.originalUrl));
 offersRouter.get(`/add`, (req, res) => res.send(req.originalUrl));
+offersRouter.get(`/edit/:id`, (req, res) => res.send(req.originalUrl));
 offersRouter.get(`/:id`, (req, res) => res.send(req.originalUrl));
 
 module.exports = offersRouter;
