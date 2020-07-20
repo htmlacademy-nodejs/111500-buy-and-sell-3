ed9904b67548b20d16e759f93bb190dce94e428f
novitckas <novitckas@selectel.ru>
2020-07-20 16:07:13 +0300

    Поправил пути для генерации моков

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -19,7 +19,7 @@ const typeList = [
   `offer`,
   `sale`
 ];
-const TEXTS_FOLDER = `../../data`;
+const TEXTS_FOLDER = `../../../data`;
 
 const generateOffer = async (value) => {
   const objectsInArrayNumber = Number.parseInt(value, 10) || DEFAULT_OBJECTS_NUMBER;
@@ -61,7 +61,7 @@ const randomSliceArray = async (fileName, maxLength) => {
 };
 
 const getArrayFromFile = async (fileName) => {
-  const list = await readFile(path.resolve(`${TEXTS_FOLDER}/`, fileName), `utf8`);
+  const list = await readFile(path.resolve(__dirname, `${TEXTS_FOLDER}/`, fileName), `utf8`);
   return list.split(os.EOL).filter((i) => i);
 };
 
