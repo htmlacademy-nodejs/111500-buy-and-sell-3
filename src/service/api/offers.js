99dcf8d119cda716bbdbfc615ca50ee38ffbb515
novitckas <novitckas@selectel.ru>
2020-08-30 23:32:30 +0300

    Добавил обработку PUT /:offerId и DELETE /:offerId

--- src/service/api/offers.js
+++ src/service/api/offers.js
@@ -8,6 +8,7 @@ const offersRouter = new Router();
 
 module.exports = (app, offerService) => {
   app.use(`/offers`, offersRouter);
+
   offersRouter.get(`/`, async (req, res) => {
     const offers = offerService.getAll();
     res.status(HTTP_CODE.OK).json(offers);
@@ -17,7 +18,7 @@ module.exports = (app, offerService) => {
     const offer = offerService.getOne(offerId);
 
     if (!offer) {
-      res.status(HTTP_CODE.NOT_FOUND).json(`Not found with id ${offerId}`);
+      res.status(HTTP_CODE.NOT_FOUND).json(`Offer not found. id ${offerId}`);
     } else {
       res.status(HTTP_CODE.OK).json(offerService.getOne(offerId));
     }
@@ -25,6 +26,24 @@ module.exports = (app, offerService) => {
   offersRouter.post(`/`, async (req, res) => {
     const createdOffer = offerService.create(req.body);
     res.status(HTTP_CODE.CREATED).json(createdOffer);
+  });
+  offersRouter.put(`/:offerId`, async (req, res) => {
+    const {offerId} = req.params;
+    const offer = offerService.getOne(offerId);
+    if (offer) {
+      res.status(HTTP_CODE.OK).json(Object.assign(offer, req.body));
+    } else {
+      res.status(HTTP_CODE.NOT_FOUND).json(`Offer not found. id ${offerId}`);
+    }
+  });
+  offersRouter.delete(`/:offerId`, async (req, res) => {
+    const {offerId} = req.params;
+    const isDeleted = offerService.delete(offerId);
+    if (isDeleted) {
+      res.status(HTTP_CODE.OK);
+    } else {
+      res.status(HTTP_CODE.NOT_FOUND).json(`Can't delete. Offer not found. id ${offerId}`);
+    }
   });
 };
 
--- src/service/data-service/offers.js
+++ src/service/data-service/offers.js
@@ -17,6 +17,29 @@ class OffersService {
   create(offer) {
     this._offers.push(Object.assign(offer, {id: nanoid()}));
   }
+
+  update(id, fieldsToUpdate) {
+    let offer = this.getOne(id);
+    offer = Object.assign(offer, fieldsToUpdate);
+    return offer;
+  }
+
+  delete(id) {
+    let isDeleted = false;
+    this._offers = this._offers.filter((i) => {
+      if (i.id === id) {
+        isDeleted = true;
+        return false;
+      }
+      return true;
+    });
+    return isDeleted;
+  }
+
+  getComments(offerId) {
+    const offer = this._offers.find((i) => i.id === offerId);
+    return offer ? offer.comments : null;
+  }
 }
 
 module.exports = OffersService;
